6acc10bcd746d41404b03fcba52d6ecc521565be
Robert Chiniquy <robert.chiniquy@gmail.com>
2021-12-21 13:37:17 -0800

    HACK: quick fixes to make this work with the latest libssl-dev .deb

--- download-libssl-dev.sh
+++ download-libssl-dev.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e
+set -ex
 
 mkdir -p target
 cd target
@@ -8,6 +8,8 @@ cd target
 # From time to time the download link gets taken down and needs to be replaced with a new one.
 # Here's where the current download link was found:
 #  https://packages.debian.org/buster/amd64/libssl-dev/download
-curl -O http://ftp.us.debian.org/debian/pool/main/o/openssl/libssl-dev_1.1.1d-0+deb10u6_amd64.deb
-ar p libssl-dev_1.1.1d-0+deb10u6_amd64.deb data.tar.xz | tar xvf -
-rm -rf libssl-dev_1.1.1d-0+deb10u6_amd64.deb
+curl -O http://ftp.us.debian.org/debian/pool/main/o/openssl/libssl-dev_1.1.1d-0+deb10u7_amd64.deb
+ar p libssl-dev_1.1.1d-0+deb10u7_amd64.deb data.tar.xz | tar xvf -
+rm -rf libssl-dev_1.1.1d-0+deb10u7_amd64.deb
+
+cp `find . -name opensslconf.h` ./target/usr/include/openssl/
